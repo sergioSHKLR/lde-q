d0cec13b7d004b91dc0cff86d1351a2808e4128b
sergioSHKLR <sergioSHKLR@users.noreply.github.com>
2026-09-25 10:08:24 +0000

    Início: drop sparkles above the title.

--- app.js
+++ app.js
@@ -393,7 +393,6 @@ function paintHome() {
   return `<main class="page index">
       <div class="index-head">
         <button class="iconbtn settings-corner" data-act="open-settings" title="${t("settings")}"><i data-icon="settings"></i></button>
-        <div class="kicker"><i data-icon="sparkles" data-icon-size="22" class="brand"></i></div>
         <h1 class="book-title">${t("book")}</h1>
         <p class="tagline">${t("study")}</p>
         <div class="index-actions">
